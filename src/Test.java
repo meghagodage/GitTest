d64870446b28cde83a45cf48a9ee43e37099f39d
Atish <Atish@shree>
2020-02-03 15:59:38 +0530

    First  Commit...

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String name="Megha";
+		if(name=="Megha")
+		{
+		   System.out.println( "Do study");
+		   
+		}
+		else
+		{
+			System.out.println("Play ");
+		}
+	}
+
+}
